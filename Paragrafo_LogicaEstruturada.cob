0bc752da05ca13a256fca21f45e91f287d7025ad
luan magalhães <luan3642@hotmail.com>
2021-01-23 10:02:35 -0300

    utilizando parágrafo e lógica estruturada, para deixar o código mais clean, ajundando a manter uma manutenção de fácil acesso e entendimento

---
+++ Paragrafo_LogicaEstruturada.cob
@@ -0,0 +1,41 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PARAGRAFO.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+       77 WRK-NOTA1 PIC 9(02)V99 VALUE ZEROS.
+       77 WRK-NOTA2 PIC 9(02)V99 VALUE ZEROS.
+       77 WRK-MEDIA PIC 9(02)V99 VALUE ZEROS.
+
+       PROCEDURE DIVISION.
+       0001-PRINCIPAL.
+           PERFORM 0100-INICIALIZAR.
+           IF WRK-NOTA1 >0 AND WRK-NOTA2 >0
+               PERFORM 0200-PROCESSAR.
+           PERFORM 0300-FINALIZAR.
+           STOP RUN.
+      * PARÁGRAFOS E SESSÕES
+       0100-INICIALIZAR.
+           ACCEPT WRK-NOTA1.
+           ACCEPT WRK-NOTA2.
+       0200-PROCESSAR.
+           COMPUTE WRK-MEDIA = (WRK-NOTA1 + WRK-NOTA2) /2.
+           DISPLAY 'MEDIA ' WRK-MEDIA.
+             IF WRK-MEDIA >=6
+                 DISPLAY 'APROVADO'
+             ELSE
+                 IF WRK-MEDIA >=2
+                     DISPLAY 'RECUPERACAO'
+                 ELSE
+                     DISPLAY 'REPROVADO'
+                 END-IF
+             END-IF.
+       0200-PROCESSAR-FIM.
+       0300-FINALIZAR.
+           DISPLAY '----------------------'.
+           DISPLAY 'FINAL DE PROCESSAMENTO'.
